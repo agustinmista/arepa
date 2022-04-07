93c5d779dca19e040cfe181392bfeda1fe1952c5
Agustin Mista <mista@chalmers.se>
2022-04-07 20:56:57 +0200

    Added opaque variable type

---
+++ src/Var.hs
@@ -0,0 +1,34 @@
+module Var 
+  ( Var
+  , mkVar
+  , varName 
+  ) where
+
+import Data.String
+
+import Data.Text (Text)
+import Data.Text qualified as Text
+
+----------------------------------------
+-- Variables
+----------------------------------------
+
+-- A simple variable opaque type for now. 
+-- We will likely need to expand it in the future.
+
+newtype Var = Var Text 
+  deriving (Show, Read, Eq, Ord) 
+
+-- Constructors/destructors
+
+mkVar :: Text -> Var
+mkVar = Var
+
+varName :: Var -> Text 
+varName (Var t) = t
+
+-- This instance let us write variables directly as strings:
+-- >>> "foo" :: Var) 
+-- "Var \"foo\""
+instance IsString Var where
+  fromString s = Var (Text.pack s)
