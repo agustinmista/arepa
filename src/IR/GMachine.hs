ba9f221c6a8a3cbb75872b2afc710d1bc9625220
Alejandro Gómez-Londoño <agomezl@pm.me>
2022-04-14 18:18:19 +0200

    Add G-Machine interpreter

--- src/IR/GMachine.hs
+++ src/IR/GMachine.hs
@@ -82,3 +82,95 @@ hAllocGm node h = Map.insert free node h
   where
     free = head $ filter (flip Map.notMember h) [1..]
 
+---------------------------
+-- G-Machine interpreter --
+---------------------------
+
+-- | Given a state returns runs the G-Machine and returns a list with
+--   each intermediate state with `last . evalGm` being the final result
+--   (if any)
+evalGm :: GmState -> [GmState]
+evalGm state = state : restState
+  where
+    restState | gmFinal (code state) = []
+              | otherwise            = evalGm nextState
+    nextState  = stepGm state
+    gmFinal [] = True
+    gmFinal _  = False
+
+-- | A single instruction transition from the current state to the next one
+stepGm :: GmState -> GmState
+stepGm state = doInst i (state { code = is})
+  where
+    i:is = code state
+    doInst  Unwind        = doUnwindGm
+    doInst (PushGlobal s) = doPushGlobalGm s
+    doInst (PushInt n)    = doPushIntGm n
+    doInst (Push n)       = doPushGm n
+    doInst  MkApp         = doAppGm
+    doInst (Slide n)      = doSlideGm n
+
+-- | Fetch the address of the given function from `globals` and
+--   place it at the top of the stack
+doPushGlobalGm :: String  -> GmState -> GmState
+doPushGlobalGm s state =
+  case Map.lookup s (globals state) of
+    Nothing  -> error $ "ERROR: Function " ++ s ++ " does not exists"
+    Just n   -> state { stack = n : stack state }
+
+-- | Allocate some heap space for an integer an
+--   place it at the top of the stack
+doPushIntGm :: Int -> GmState -> GmState
+doPushIntGm i state = updateHeap state $ hAllocGm (NNum i)
+
+-- | Place the argument at the given offset
+--   (starting  from 0 before the top of the stack)
+--   and place it at the top of the stack. It must be the
+--   case that the node at the given offset is an application and as
+--   such we take its operand (the argument).
+doPushGm :: Int -> GmState -> GmState
+doPushGm n state =
+  case hLookupGm h addr of
+    NApp _ argN -> pushGm state argN
+    _           -> error $ "ERROR: no application at " ++ show (n+1,addr)
+  where
+    h  = heap state
+    s  = stack state
+    addr = if n + 1 < length s
+           then  s !! n + 1
+           else error "ERROR: There are not enough argument in the stack"
+
+-- | Creates a application from the two topmost elements of the stack
+--   as a operator (1st element) applied to an operant (2nd element).
+doAppGm :: GmState -> GmState
+doAppGm state = updateHeap state' $ hAllocGm (NApp a1 a2)
+  where
+    a1:a2:rest = stack state
+    state'     = state { stack = rest }
+
+-- | Takes the top of the stack and slides it a given offset.
+--   This occurs when we must replace a function application in the stack
+--   with its graph representation.
+doSlideGm :: Int -> GmState -> GmState
+doSlideGm n state = state { stack = head s : drop n (tail s)}
+  where s = stack state
+
+-- | Unwind takes the topmost element in the stack determines what to do next:
+--   (NNum) Consume the instruction and return the state (We are done)
+--   (NApp) Unfold the application and push the operator to the stack
+--   (NGlobal) At this point all argument for the function call should be on
+--             the stack, check for this and start executing the function code
+--
+-- Note: it is assumed tha Unwind is the last instruction in any program.
+doUnwindGm :: GmState -> GmState
+doUnwindGm state = newState (hLookupGm h a)
+  where
+    -- Current Heap & Stack
+    a:as = stack state
+    h    = heap state
+    -- What to do depending on what is in the top of the stack
+    newState (NNum _)         = state { code = [] }
+    newState (NApp a1 _)     = pushGm state a1
+    newState (NGlobal d fCode)
+      | length  as < d = error "ERROR: Not enough arguments"
+      | otherwise      = state { code = fCode }
