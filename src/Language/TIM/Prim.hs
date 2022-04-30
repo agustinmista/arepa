fcbeb263bc6cad4a3ffabee5be4c87dc7d9e278f
Agustin Mista <mista@chalmers.se>
2022-04-30 21:49:11 +0200

    NIT

--- src/Language/TIM/Prim.hs
+++ src/Language/TIM/Prim.hs
@@ -4,10 +4,12 @@ module Language.TIM.Prim
   , module Language.TIM.Prim.Types
   ) where
 
+import System.FilePath
+
 import Language.TIM.Prim.Types
 import Language.TIM.Prim.TH
 
 ----------------------------------------
 -- Mining primitive operations from the RTS
 
-mkPrimitives "primitives" "rts/include/prim.h"
+mkPrimitives "primitives" ("rts" </> "include" </> "prim.h")
