1b45c8241a02a045f0c4599b99cf7baf443f9047
Agustin Mista <mista@chalmers.se>
2022-04-30 21:48:56 +0200

    Added Z-encoding to names

--- src/Data/Name.hs
+++ src/Data/Name.hs
@@ -6,6 +6,7 @@ import Data.Text.Lazy (Text)
 import Data.Text.Lazy qualified as Text
 
 import Prettyprinter
+import Text.Encoding.Z
 
 ----------------------------------------
 -- Names
@@ -24,6 +25,9 @@ mkNameWithNum prefix n = Name (prefix <> Text.pack (show n))
 
 fromName :: IsString a => Name -> a
 fromName (Name t) = fromString (Text.unpack t)
+
+zEncode :: Name -> Name
+zEncode name = Name (Text.pack (zEncodeString (fromName name)))
 
 -- This instance let us write variables directly as strings
 instance IsString Name where
