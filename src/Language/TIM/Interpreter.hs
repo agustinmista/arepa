2ab8511215b56d9c760846646db5e63b752e0311
Agustin Mista <mista@chalmers.se>
2022-05-06 14:05:24 +0200

    Adapted interpreter to work with new take and move

--- src/Language/TIM/Interpreter.hs
+++ src/Language/TIM/Interpreter.hs
@@ -55,23 +55,27 @@ stepTIM :: TIM ()
 stepTIM = do
   instr <- fetchInstr
   case instr of
-    TakeArgI n -> do
-      closures <- takeArgStack n
-      ptr <- allocFrame (mkFrame closures)
+    TakeArgI t n -> do
+      stackClosures <- takeArgStack n
+      let localClosures = replicate (t - n) dummyClosure
+      ptr <- allocFrame (mkFrame (stackClosures <> localClosures))
       setFramePtr ptr
-    EnterI mode -> do
-      closure <- derefClosure mode
-      setCode (closure_code closure)
-      setFramePtr (closure_frame closure)
     PushArgI mode -> do
       closure <- derefClosure mode
       pushArgStack closure
     PushValueI mode -> do
       pushValueStack mode
-    CallI name -> do
-      prim <- lookupPrimOp name
-      operateOnValueStack (prim_arity prim) (prim_runner prim)
+    EnterI mode -> do
+      closure <- derefClosure mode
+      setCode (closure_code closure)
+      setFramePtr (closure_frame closure)
+    MoveI n mode -> do
+      closure <- derefClosure mode
+      updateClosure n closure
     ReturnI -> do
       [closure] <- takeArgStack 1
       setCode (closure_code closure)
       setFramePtr (closure_frame closure)
+    CallI name -> do
+      prim <- lookupPrimOp name
+      operateOnValueStack (prim_arity prim) (prim_runner prim)
