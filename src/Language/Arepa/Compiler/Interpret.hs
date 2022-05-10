0dc7dac4fd2b3aa2a763a2bf8b1a94e091b5f6d0
Agustin Mista <mista@chalmers.se>
2022-05-10 21:21:20 +0200

    Integrated custom RTS stdout into Haskell interpreter

--- src/Language/Arepa/Compiler/Interpret.hs
+++ src/Language/Arepa/Compiler/Interpret.hs
@@ -12,14 +12,14 @@ import Language.Arepa.Compiler.Monad
 -- Interpretion of translated modules
 ----------------------------------------
 
--- Interpret a TIM code store, invoking some function
+-- Interpret a TIM code store, invoking the entry point function
 interpretCodeStore :: MonadArepa m => CodeStore -> m ()
 interpretCodeStore store = do
   entry <- lookupCompilerOption optEntryPoint
+  stdout <- lookupCompilerOption optInterpretStdout
   let fun = mkName (fromMaybe "main" entry)
   whenVerbose $ debug ("Interpreting code store [entry=" <> prettyPrint fun <> "]")
-  (res, trace) <- liftIO $ do
-    runTIM store $ invokeFunction fun []
+  (res, trace) <- liftIO $ runTIM stdout store $ invokeFunction fun []
   whenVerbose $ dump "Interpreter intermediate states" (prettyPrint trace)
   case res of
     Left err -> do
